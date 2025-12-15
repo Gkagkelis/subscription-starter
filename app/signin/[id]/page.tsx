d43083d4b4e47047ffb519a52017b5a2eed79d44
Gkagkelis <viewscoperesearch@gmail.com>
2025-12-15 23:35:55 +0200

    Update page.tsx

--- app/signin/[id]/page.tsx
+++ app/signin/[id]/page.tsx
@@ -51,7 +51,9 @@ export default async function SignIn({
   }
 
   return (
-   <div className="flex justify-center pb-12 ">
+    <div className="flex justify-center height-screen-helper">
+      <div className="flex flex-col justify-between max-w-lg p-3 m-auto w-80">
+        <div className="flex justify-center pb-12">
           <Image 
             src="/axiprova.jpeg" 
             alt="Axiprova" 
