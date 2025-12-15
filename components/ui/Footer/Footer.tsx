3c4bc376f926a1c22adb1b365923040efb6e9fdd
Gkagkelis <viewscoperesearch@gmail.com>
2025-12-16 01:46:18 +0200

    Update Footer.tsx

--- components/ui/Footer/Footer.tsx
+++ components/ui/Footer/Footer.tsx
@@ -3,9 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="mx-auto max-w-[1920px] px-6 bg-zinc-900">
-      {/* Top footer area */}
-      <div className="grid grid-cols-1 gap-8 py-12 text-white transition-colors duration-150 border-b lg:grid-cols-12 border-zinc-600 bg-zinc-900">
-        {/* Brand (NO LOGO) */}
+      <div className="grid grid-cols-1 gap-8 py-12 text-white transition-colors duration-150 lg:grid-cols-12 bg-zinc-900">
+        {/* Brand */}
         <div className="col-span-1 lg:col-span-4">
           <Link href="/" className="flex items-center flex-initial md:mr-24">
             <span className="flex flex-col leading-tight">
@@ -63,24 +62,6 @@ export default function Footer() {
               </a>
             </div>
           </div>
-        </div>
-      </div>
-
-      {/* Bottom footer area */}
-      <div className="flex flex-col items-center justify-between py-10 space-y-4 md:flex-row bg-zinc-900">
-        <div className="text-sm text-zinc-300">Axiprova 2025</div>
-
-        <div className="text-sm text-zinc-400 text-center">
-          Your AI advisor for culture &amp; the creative industries
-        </div>
-
-        <div className="text-sm">
-          <a
-            href="mailto:viewscoperesearch@gmail.com"
-            className="text-zinc-300 hover:text-zinc-200 transition"
-          >
-            viewscoperesearch@gmail.com
-          </a>
         </div>
       </div>
     </footer>
