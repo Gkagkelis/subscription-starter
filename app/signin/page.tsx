12b71c8039f35a2b07b779e03c58a6491db2b82a
Gkagkelis <viewscoperesearch@gmail.com>
2025-12-17 17:26:16 +0200

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -2,10 +2,17 @@ import { redirect } from 'next/navigation';
 import { getDefaultSignInView } from '@/utils/auth-helpers/settings';
 import { cookies } from 'next/headers';
 
-export default function SignIn() {
+export default function SignIn({
+  searchParams
+}: {
+  searchParams?: { next?: string };
+}) {
   const preferredSignInView =
     cookies().get('preferredSignInView')?.value || null;
+
   const defaultView = getDefaultSignInView(preferredSignInView);
 
-  return redirect(`/signin/${defaultView}`);
+  const next = searchParams?.next ? encodeURIComponent(searchParams.next) : '';
+
+  return redirect(next ? `/signin/${defaultView}?next=${next}` : `/signin/${defaultView}`);
 }
