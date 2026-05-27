273053fd4a2f5798f6ff49dcfdf05b96086221fc
Gkagkelis <viewscoperesearch@gmail.com>
2026-05-28 02:22:33 +0300

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,18 +2,18 @@ import { createClient } from '@/utils/supabase/server';
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 import { getErrorRedirect, getStatusRedirect } from '@/utils/helpers';
-
+ 
 export async function GET(request: NextRequest) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the `@supabase/ssr` package. It exchanges an auth code for the user's session.
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-
+ 
   if (code) {
     const supabase = createClient();
-
+ 
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-
+ 
     if (error) {
       return NextResponse.redirect(
         getErrorRedirect(
@@ -24,13 +24,14 @@ export async function GET(request: NextRequest) {
       );
     }
   }
-
+ 
   // URL to redirect to after sign in process completes
   return NextResponse.redirect(
     getStatusRedirect(
-      `${requestUrl.origin}/account`,
+      `${requestUrl.origin}/strategy-room`,
       'Success!',
       'You are now signed in.'
     )
   );
 }
+ 
